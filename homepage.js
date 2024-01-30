eeb8db46de57999f9fc44418f963476e0da0018d
Barb02 <98841800+Barb02@users.noreply.github.com>
2024-01-30 19:59:42 -0300

    language change done

--- homepage.js
+++ homepage.js
@@ -1,28 +1,77 @@
-function calculaAmor(){
-    var nome1 = document.getElementById("nome1").value;
-    var nome2 = document.getElementById("nome2").value;
-    if(nome1.length > 0 && nome2.length > 0){
+
+var lang = "en"
+
+$('[lang]').hide(); // hide all lang attributes on start.
+$('[lang="en"]').show(); 
+$('#en-switch').on("click", function () {
+    
+    $('[lang]').hide();
+    $('[lang="en"]').show();
+    lang = "en";
+});
+$('#pt-switch').on("click", function () {
+    $('[lang]').hide();
+    $('[lang="pt"]').show();
+    lang = "pt";
+});
+
+function calculateLove(){
+    if(lang == "pt"){
+        var name1 = document.getElementById("name1").value;
+        var name2 = document.getElementById("name2").value; 
+    }
+    else{
+        var name1 = document.getElementById("name1en").value;
+        var name2 = document.getElementById("name2en").value; 
+    }
+    
+    if(name1.length > 0 && name2.length > 0){
         var num = Math.round(Math.random()*100);
         document.getElementById("modalTitle").innerHTML = num + "%";
 
         if(num <= 20){
-            document.getElementById("modalBody").innerHTML = "É...não foi dessa vez. Sinto muito, mas o amor não é para vocês.";
+            if(lang == "pt"){
+                document.getElementById("modalBody").innerHTML = "É... não foi dessa vez. Sinto muito, mas o amor não é para vocês.";
+            }
+            else{
+                document.getElementById("modalBody").innerHTML = "Well... not in this lifetime. I'm sorry, but love isn't for you two.";
+            }
             $("#gif").attr("src","img_bundle/guaxinim_algodao_doce.gif");
         }
         else if(num > 20 && num <= 40){
-            document.getElementById("modalBody").innerHTML = "O que caberia melhor aqui é uma amizade. Mais que amigos, friends.";
+            if(lang == "pt"){
+                document.getElementById("modalBody").innerHTML = "O que caberia melhor aqui é uma amizade. Mais que amigos, friends.";
+            }
+            else{
+                document.getElementById("modalBody").innerHTML = "A friendship would fit best. #BrosForLife";
+            }
             $("#gif").attr("src","img_bundle/hoje_nao.gif");
         }
         else if(num > 40 && num <= 60){
-            document.getElementById("modalBody").innerHTML = "Tem potencial...mas será uma longa jornada. Boa sorte.";
+            if(lang == "pt"){
+                document.getElementById("modalBody").innerHTML = "Vejo potencial, mas será uma longa jornada. Boa sorte, amigo.";
+            }
+            else{
+                document.getElementById("modalBody").innerHTML = "I see potential, but it will be a long journey. Good luck, my friend.";
+            }
             $("#gif").attr("src","img_bundle/guaxinim_com_cuidado.gif");
         }
         else if(num > 60 && num <= 80){
-            document.getElementById("modalBody").innerHTML = "Formam um belo casal, sem dúvida.";
+            if(lang == "pt"){
+                document.getElementById("modalBody").innerHTML = "Formam um belo casal, sem dúvida!";
+            }
+            else{
+                document.getElementById("modalBody").innerHTML = "A cute couple, for sure!";
+            }
             $("#gif").attr("src","img_bundle/bate_aqui.gif");
         }
         else{
-            document.getElementById("modalBody").innerHTML = "Parabéns, você encontrou sua alma gêmea!!";
+            if(lang == "pt"){
+                document.getElementById("modalBody").innerHTML = "Parabéns! Você encontrou sua alma gêmea!!";
+            }
+            else{
+                document.getElementById("modalBody").innerHTML = "Congrats! You've found your soulmate!!";
+            }
             $("#gif").attr("src","img_bundle/amor.gif");
         }
         $('#resultsModal').modal('show');
